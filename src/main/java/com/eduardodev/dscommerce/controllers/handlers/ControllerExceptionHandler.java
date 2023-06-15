397c99b4aebf883c10253ed42ace566142f45ce7
eduardodev-1 <122590889+eduardodev-1@users.noreply.github.com>
2023-06-14 22:28:02 -0300

    Bean Validation

--- src/main/java/com/eduardodev/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/eduardodev/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -2,29 +2,45 @@ package com.eduardodev.dscommerce.controllers.handlers;
 
 import java.time.Instant;
 
-import com.eduardodev.dscommerce.services.exceptions.DataBaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.eduardodev.dscommerce.dto.CustomError;
+import com.eduardodev.dscommerce.dto.ValidationError;
+import com.eduardodev.dscommerce.services.exceptions.DataBaseException;
 import com.eduardodev.dscommerce.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-    @ExceptionHandler(DataBaseException.class)
-    public ResponseEntity<CustomError> database(DataBaseException e, HttpServletRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
-    return ResponseEntity.status(status).body(err);
-    }
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-        HttpStatus status = HttpStatus.NOT_FOUND;
-        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
-        return ResponseEntity.status(status).body(err);
-    }
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<CustomError> database(DataBaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e,
+			HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(status).body(err);
+	}
 }
--- src/main/java/com/eduardodev/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/eduardodev/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,25 @@
 package com.eduardodev.dscommerce.dto;
 
+
 import com.eduardodev.dscommerce.entities.Product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
 	private Long id;
+	
+	@Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	
+	@Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String description;
+	
+	@Positive(message = "O preço não pode ser menor que zero")
 	private Double price;
 	private String imgUrl;
 
@@ -18,6 +31,7 @@ public class ProductDTO {
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
+
 	public ProductDTO(Product entity) {
 		super();
 		this.id = entity.getId();
